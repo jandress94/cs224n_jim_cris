497a231b4e4154e833729077b7fc4aa6ad90f07f
Jim <jandress@stanford.edu>
2017-03-10 22:29:34 -0800

    Fixes for the qa_data / glove / vocab.dat issue

--- code/qa_data.py
+++ code/qa_data.py
@@ -63,6 +63,8 @@ def process_glove(args, vocab_list, save_path, size=4e5):
     :return:
     """
     if not gfile.Exists(save_path + ".npz"):
+        vocab_dict = {vocab_list[i] : i for i in xrange(len(vocab_list))}
+
         glove_path = os.path.join(args.glove_dir, "glove.6B.{}d.txt".format(args.glove_dim))
         glove = np.zeros((len(vocab_list), args.glove_dim))
         not_found = 0
@@ -71,17 +73,25 @@ def process_glove(args, vocab_list, save_path, size=4e5):
                 array = line.lstrip().rstrip().split(" ")
                 word = array[0]
                 vector = list(map(float, array[1:]))
-                if word in vocab_list:
-                    idx = vocab_list.index(word)
+                # if word in vocab_list:
+                if word in vocab_dict:
+                    # idx = vocab_list.index(word)
+                    idx = vocab_dict[word]
                     glove[idx, :] = vector
-                elif word.capitalize() in vocab_list:
-                    idx = vocab_list.index(word.capitalize())
+                # elif word.capitalize() in vocab_list:
+                elif word.capitalize() in vocab_dict:
+                    # idx = vocab_list.index(word.capitalize())
+                    idx = vocab_dict[word.capitalize()]
                     glove[idx, :] = vector
-                elif word.lower() in vocab_list:
-                    idx = vocab_list.index(word.lower())
+                # elif word.lower() in vocab_list:
+                elif word.lower() in vocab_dict:
+                    # idx = vocab_list.index(word.lower())
+                    idx = vocab_dict[word.lower()]
                     glove[idx, :] = vector
-                elif word.upper() in vocab_list:
-                    idx = vocab_list.index(word.upper())
+                # elif word.upper() in vocab_list:
+                elif word.upper() in vocab_dict:
+                    # idx = vocab_list.index(word.upper())
+                    idx = vocab_dict[word.upper()]
                     glove[idx, :] = vector
                 else:
                     not_found += 1
@@ -104,10 +114,10 @@ def create_vocabulary(vocabulary_path, data_paths, tokenizer=None):
                         print("processing line %d" % counter)
                     tokens = tokenizer(line) if tokenizer else basic_tokenizer(line)
                     for w in tokens:
-                        if w in vocab:
-                            vocab[w] += 1
+                        if w.lower() in vocab:
+                            vocab[w.lower()] += 1
                         else:
-                            vocab[w] = 1
+                            vocab[w.lower()] = 1
         vocab_list = _START_VOCAB + sorted(vocab, key=vocab.get, reverse=True)
         print("Vocabulary size: %d" % len(vocab_list))
         with gfile.GFile(vocabulary_path, mode="wb") as vocab_file:
